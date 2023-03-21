b82dc346a35513f8daa07cd3c9845de01146808a
Clement Dejoie <clement.dejoie@creativelift.ai>
2023-03-21 15:56:27 +0100

    Use Counter instead of dict and one-time sorting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple, List
+from collections import Counter
+from typing import Counter as CounterType, Tuple, List
 
 
 def compute_words_frequency(sentence: str, limit: int) -> List[Tuple[str, int]]:
@@ -7,15 +8,9 @@ def compute_words_frequency(sentence: str, limit: int) -> List[Tuple[str, int]]:
 
     words = sentence.split()
 
-    words_frequency: Dict[str, int] = {}
-    for word in words:
-        if word in words_frequency:
-            words_frequency[word] += 1
-        else:
-            words_frequency[word] = 1
+    words_frequency: CounterType[str] = Counter(words)
 
     words_frequency_results = list(words_frequency.items())
-    words_frequency_results.sort(key=lambda x: x[0])
-    words_frequency_results.sort(key=lambda x: x[1], reverse=True)
+    words_frequency_results.sort(key=lambda x: (-x[1], x[0]))
 
     return words_frequency_results[:limit]
